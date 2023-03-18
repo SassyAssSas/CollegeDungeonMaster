85260dde92c81aaeaaee7309e7b0bd7766eeacc0
Violoncello <rrree3412@gmail.com>
2023-03-18 13:59:11 +0200

    Collectables update
    Collectables no longer enter walls

--- CollegeDungeonMaster/Assets/Scripts/Entities/Items/CollectableEntity.cs
+++ CollegeDungeonMaster/Assets/Scripts/Entities/Items/CollectableEntity.cs
@@ -7,7 +7,11 @@ public class CollectableEntity : MonoBehaviour {
    [field: SerializeField] public CollectableType Type { get; private set; }
 
    private const float jumpHeight = 0.5f;
+   private const float jumpSpeed = 2f;
    private const float offset = 0.3f;
+
+   private const float followPlayerDistance = 1.5f;
+   private const float followPlayerSpeed = 3f;
 
    private void Start() {
       StopAllCoroutines();
@@ -16,8 +20,8 @@ public class CollectableEntity : MonoBehaviour {
    }
 
    private void Update() {
-      if (Vector2.Distance(Player.Instance.transform.position, transform.position) < 1f) {
-         transform.position += 2 * Time.deltaTime * (Player.Instance.transform.position - transform.position).normalized;
+      if (Vector2.Distance(Player.Instance.transform.position, transform.position) < followPlayerDistance) {
+         transform.position += followPlayerSpeed * Time.deltaTime * (Player.Instance.transform.position - transform.position).normalized;
       }
    }
 
@@ -30,7 +34,7 @@ public class CollectableEntity : MonoBehaviour {
 
          _object.transform.localPosition = position;
 
-         elapsedTime += Time.deltaTime * 2f;
+         elapsedTime += Time.deltaTime * jumpSpeed;
 
          yield return null;
       }
@@ -41,13 +45,19 @@ public class CollectableEntity : MonoBehaviour {
       var target = transform.position + Quaternion.Euler(0, 0, angle) * new Vector2(Random.value, 0f);
 
       var elapsedTime = 0f;
-      while (transform.position != target && Vector2.Distance(Player.Instance.transform.position, transform.position) >= 1f) {
+      while (Vector2.Distance(transform.position, target) > 0.1f && Vector2.Distance(Player.Instance.transform.position, transform.position) >= followPlayerDistance) {
          transform.position = Vector3.Lerp(transform.position, target, Time.deltaTime);  
 
          elapsedTime += Time.deltaTime;
 
          yield return null;
       }
+   }
+
+   private void OnCollisionEnter2D(Collision2D collision) {
+      int wallLayer = 8;
+      if (collision.collider.gameObject.layer == wallLayer)
+         StopCoroutine(Move());   
    }
 
    public enum CollectableType {
